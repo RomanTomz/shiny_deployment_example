80c536ea40f1e55fdf06c19e18846d5fdf42a786
Tommaso Di Marcello <t.dimarcello@gmail.com>
2021-04-29 17:08:16 +0100

    init&run

---
+++ app.R
@@ -0,0 +1,30 @@
+install.packages('shiny')
+install.packages('htmltools')
+
+library(shiny)
+
+
+ui <- fluidPage(
+  sliderInput(inputId = "num", 
+    label = "Choose a number", 
+    value = 25, min = 1, max = 100),
+  plotOutput("punk"),
+  
+  sliderInput(inputId = "num2", 
+              label = "Choose a number", 
+              value = 100, min = 1, max = 1000),
+  plotOutput("hist")
+  
+  )
+
+server <- function(input, output) {
+  output$punk <- renderPlot({
+    hist(rnorm(input$num))
+  })
+  
+  output$hist <- renderPlot({
+    hist(rnorm(input$num2))
+  })
+}
+
+shinyApp(ui = ui, server = server)
